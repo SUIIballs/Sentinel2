98e16bf763fad7ae9ae03c678d4e039d572787cf
Akshat Prasad <akshatprasad25@gmail.com>
2026-09-18 11:11:48 +0530

    Fix mobile backend proxy configuration

--- Mobile-Web-App/vite.config.ts
+++ Mobile-Web-App/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 
     proxy: {
       "/api": {
-        target: "http://192.168.1.7:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
